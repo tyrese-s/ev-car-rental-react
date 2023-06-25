3fda8bb48dd18e68c6322ff42f14db534e81c992
tyrese-s <tyrese.sylvester@yahoo.com>
2023-06-25 13:49:26 +0100

    some responsive styling and page title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,22 +20,29 @@ export default function Home () {
     // console.log(carModel, location);
     return (
         <div class="mb-96">
-        <div class="columns-2 mt-40">
-            <div class=" grid justify-items-start pl-60">
-                <h1 class="text-7xl font-bold text-left">Save <span class="text-red-500">big</span> with our electric car rental</h1>
+        <div class="sm:flex sm:columns-1 md:columns-2 mt-40">
+            <div class="grid justify-items-start px-5 md:pl-20 lg:pl-40">
+                <h1 class="text-3xl md:text-7xl font-bold text-left">Save <span class="text-red-500">big</span> with our electric car rental</h1>
                 <p class="text-left pt-2">Your environmentally friendly car rental service</p>
                 <p class="text-left pt-4">Rent the car you want, unbeatable prices with flexible pick-up locations and more.</p>
                 <p class="text-left pt-4">Pick up a fully charged car.</p>
                 <div class="flex flex-row space-x-4 mt-12">
+                <a href='#carBooking'
+                onClick={e => {
+                    let carBooking = document.getElementById("carBooking")
+                    e.preventDefault()
+                    carBooking && carBooking.scrollIntoView({behavior: 'smooth'})
+                }}>
                 <Button variant='contained' size='large'>Book a car</Button>
-                <Button variant='outlined'>Learn more</Button>
+                </a>
+                <Button variant='outlined' >Learn more</Button>
                 </div>
             </div>
             <div>
-                <img src={tesla} alt="Tesla model 3"/>
+                <img src={tesla} alt="Tesla model 3" class=""/>
             </div>
         </div>
-        <div class="mt-80">
+        <div class="mt-80" id='carBooking'>
             <Grid container spacing={2} >
                 <Grid xs={12} sm={4}>
                     <InputLabel>Car model</InputLabel>
@@ -78,62 +85,6 @@ export default function Home () {
                 <Button type='submit' variant='contained' size='medium' color='primary'>Search availability</Button>
                 </Grid>
             </Grid>
-         
-       
-            {/* <h1 class="grid justify-start">Book a car</h1>
-        <div class="grid grid-cols-3 grid-rows-2">
-        <div class="flex flex-col">
-        <h2>Select car model</h2>
-                <FormControl size='medium'>
-                    <InputLabel id="car-model">Car model</InputLabel>
-                    <Select
-                    id='car-model'
-                    value={carModel}
-                    onChange={handleCarModelChange}
-                    autoWidth
-                    >
-                        <MenuItem value={'Tesla model y'}>Tesla model Y</MenuItem>
-                        <MenuItem value={'Tesla model 3'}>Tesla model 3</MenuItem>
-                        <MenuItem value={'Kia e-Niro'}>Kia e-Niro</MenuItem>
-                        <MenuItem value={'Nissan Leaf'}>Nissan Leaf</MenuItem>
-                        <MenuItem value={'Mini Electric'}>Mini Electric</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
-            <div>
-                <h2>Pick-up</h2>
-                <FormControl fullWidth>
-                    <InputLabel id="Pick-up location">Pick up location</InputLabel>
-                    <Select
-                    id='location'
-                    value={location}
-                    onChange={handleLocationChange}
-                    autoWidth>
-                        <MenuItem value={'Kensington'}>Kensington</MenuItem>
-                        <MenuItem value={'Islington'}>Islington</MenuItem>
-                        <MenuItem value={'Canary Wharf'}>Canary Wharf</MenuItem>
-                        <MenuItem value={'Whitechapel'}>Whitechapel</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
-            <div>
-                <h2>Drop-off</h2>
-                <FormControl fullWidth>
-                    <InputLabel id="car-model">Car model</InputLabel>
-                    <Select>
-                        <MenuItem></MenuItem>
-
-                    </Select>
-                </FormControl>
-            </div>
-            <div>
-                <h2>Pick-up</h2>
-            </div>
-            <div>
-                <h2>Drop-off</h2>
-            </div> */}
-
-
         </div>
         </div>
     )
